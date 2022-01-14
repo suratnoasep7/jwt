aa840fcdd2b30f1c7c9e657f25512415c46ebe88
Asep Suratno <suratnoasep7@gmail.com>
2022-01-14 15:19:45 +0700

    jwt
    jwt

---
+++ login.php
@@ -0,0 +1,79 @@
+<?php
+// Import script autoload agar bisa menggunakan library
+date_default_timezone_set("Asia/Jakarta");
+require_once('vendor/autoload.php');
+require_once('./cors.php');
+// Import library
+use Firebase\JWT\JWT;
+use Dotenv\Dotenv;
+
+// Load dotenv
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Atur jenis response
+header('Content-Type: application/json');
+
+// Cek method request
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  http_response_code(405);
+  exit();
+}
+
+// Ambil data json yang dikirim user
+$json = file_get_contents('php://input');
+$input = json_decode($json);
+
+// Jika tidak ada data email atau password
+if (!isset($input->email) || !isset($input->password)) {
+  http_response_code(400);
+  exit();
+}
+
+$user = [
+  'email' => 'johndoe@example.com',
+  'password' => 'qwerty123'
+];
+
+// Atur jenis response
+header('Content-Type: application/json');
+
+// Jika email atau password tidak sesuai
+if ($input->email !== $user['email'] || $input->password !== $user['password']) {
+  echo json_encode([
+    'success' => false,
+    'data' => null,
+    'message' => 'Email atau password tidak sesuai'
+  ]);
+  exit();
+}
+
+// Menghitung waktu kadaluarsa token. Dalam kasus ini akan terjadi setelah 15 menit
+$expired_time = time() + (15);
+
+// Buat payload dan access token
+$payload = [
+  'email' => $input->email,
+  // Di library ini wajib menambah key exp untuk mengatur masa berlaku token
+  'exp' => $expired_time
+];
+
+// Men-generate access token
+$access_token = JWT::encode($payload, $_ENV['ACCESS_TOKEN_SECRET']);
+
+// Kirim kembali ke user
+echo json_encode([
+  'success' => true,
+  'data' => [
+    'accessToken' => $access_token,
+    'expiry' => date(DATE_ISO8601, $expired_time)
+  ],
+  'message' => 'Login berhasil!'
+]);
+
+// Ubah waktu kadaluarsa lebih lama, dalam kasus ini 1 jam
+$payload['exp'] = time() + (15);
+$refresh_token = JWT::encode($payload, $_ENV['REFRESH_TOKEN_SECRET']);
+
+// Simpan refresh token di http-only cookie
+setcookie('refreshToken', $refresh_token, $payload['exp'], '', '', false, true);
---
+++ me.php
@@ -0,0 +1,50 @@
+<?php
+// Import script autoload agar bisa menggunakan library
+require_once('./vendor/autoload.php');
+require_once('./cors.php');
+// Import library
+use Firebase\JWT\JWT;
+use Dotenv\Dotenv;
+
+// Load dotenv
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Atur jenis response
+header('Content-Type: application/json');
+
+// Cek method request
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+  http_response_code(405);
+  exit();
+}
+
+$headers = getallheaders();
+
+// Periksa apakah header authorization-nya ada
+if (!isset($headers['Authorization'])) {
+  http_response_code(401);
+  exit();
+}
+
+// Mengambil token
+list(, $token) = explode(' ', $headers['Authorization']);
+
+try {
+  // Men-decode token. Dalam library ini juga sudah sekaligus memverfikasinya
+  JWT::decode($token, $_ENV['ACCESS_TOKEN_SECRET'], ['HS256']);
+  
+  $user = [
+    'name' => 'John Doe',
+    'email' => 'johndoe@example.com'
+  ];
+
+  echo json_encode($user);
+} catch (Exception $e) {
+  // Bagian ini akan jalan jika terdapat error saat JWT diverifikasi atau di-decode
+  echo json_encode([
+    'success' => false,
+    'message' => 'Access Token Expired'
+  ]);
+  exit();
+}
